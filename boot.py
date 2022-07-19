97497cad1cfb1c3dbe05475324fb7df29bb4ae2a
itischandu98 <itischandu98@gmail.com>
2022-07-18 20:56:25 -0700

    second commit

--- boot.py
+++ boot.py
@@ -3,8 +3,8 @@ import network, esp, gc
 gc.collect()
 esp.osdebug(None)
 
-ssid='jacksparrow'
-password='Qwertyuiop'
+ssid='jacksparrow'  #Network Name
+password='Password'  #Network Password
 
 station=network.WLAN(network.STA_IF)
 station.active(True)
